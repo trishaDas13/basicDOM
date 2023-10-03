374906cf0e86e746f2fa1b086fca1a31c3c88292
trishaDas13 <trisha.das1308@gmail.com>
2023-10-03 15:36:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// Use the getElementById method to retrieve the target div element.
+let div = document.getElementById("div1");
+
+// Change the background color of the fetched div element to yellow using the style.backgroundColor property. (10 Marks)
+div.style.backgroundColor = "Yellow";
+
+// Add a margin of 20 pixels to the element using the style.margin property.(10 Marks)
+div.style.margin = "20px";
+
+// Apply a padding of 10 pixels using the style.padding property.(10 Marks)
+div.style.padding = "10px";
+
+// Change the height of the element to 200 pixels using the style.height property. (10 Marks)
+div.style.height = "200px";
+
+// Modify the width of the element to 300 pixels using the style.width property.(10 Marks)
+div.style.width = "300px";
+
+// Change the font size to 18 pixels using the style.fontSize property.(10 Marks)
+div.style.fontSize = "18px";
+
+// Set the font weight to bold using the style.fontWeight property. (10 Marks)
+div.style.fontWeight = "bold";
+
+//add blue color in the font.
+div.style.color = "blue";
